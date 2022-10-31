1fa89d7b28863eb1066048ec841595916e3ed80b
Andre <apalvaradol@outlook.com>
2022-10-30 18:42:00 -0600

    conexion a postgres terminada

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,25 @@
 import express from 'express';
+import Connection from './database/connection';
 class App {
 
         public express: express.Application;
+        private connection: Connection | undefined;
 
     constructor(){
         this.express = express();
+        this.db();
     }
+    db(){
+        this.connection = new Connection();
+        this.connection.connection.sync()
+        .then(() => {
+            console.log(`Database is connected`);
+            })
+            .catch((err)=> {
+                console.log(`Error`, err);
+            })
+    }
+
     listen(port: number){
         this.express.listen(port, () => 
             console.log(`Server running in http://localhost:${port}`)
